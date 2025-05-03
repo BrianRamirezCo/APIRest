2b730411fd1183d401bda109efe1e3d9f3683a71
Ramirez Brian <tiziano0705@hotmail.com>
2025-05-03 01:14:17 -0300

    0.2

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -72,6 +72,18 @@ const login = async (req, res) => {
   }
 };
 
-const logout = async (req, res) => {};
+const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
 
-export { register };
+    return res.json({ success: true, message: "Logout successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export { register, login, logout };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/register", register);
+
+userRoutes.post("/login", login);
+
+userRoutes.post("/logout", logout);
 
 export default userRoutes;
